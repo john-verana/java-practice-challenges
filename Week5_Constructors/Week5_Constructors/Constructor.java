be62364b349264a63a9e7ecd50bcf1ffd5b2e770
John Verana <johnverana@gmail.com>
2025-08-11 05:43:26 -0400

    Create Constructor.java

---
+++ Week5_Constructors/Week5_Constructors/Constructor.java
@@ -0,0 +1,13 @@
+
+public class Constructor {
+
+	public static void main(String[] args) {
+		Movie movie1 = new Movie("Inception", "Chris Nolan", 2010);
+		Movie movie2 = new Movie("Spider-Man", "Sam Raimi", 2002); 
+		
+		movie1.displayInfo();
+		movie2.displayInfo();
+	
+	}
+
+}
