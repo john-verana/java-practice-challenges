e47b010145108f7db4cf23f0b4bf3052e0a9d2e5
John Verana <johnverana@gmail.com>
2025-08-28 20:14:18 -0400

    Add BankAccount.java for Week8_GettersSetters

---
+++ Week8_GettersSetters/BankAccount.java
@@ -0,0 +1,24 @@
+public class BankAccount {
+    private String accountHolder;
+    private double balance;
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        if (balance >= 0) {
+            this.balance = balance;
+        } else {
+            System.out.println("No negative balances allowed!");
+        }
+    }
+}
